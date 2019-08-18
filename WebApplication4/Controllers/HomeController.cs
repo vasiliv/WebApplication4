2c6ce79d741d60d3432261956794a438bc0a90ec
Vaso <Vaso@DESKTOP-SUDK15O>
2019-08-18 11:24:07 -0700

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SubmitData(Employee emp) {
+            if (ModelState.IsValid) {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ WebApplication4/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models {
+    public class Employee {
+        [Required(ErrorMessage = "Please enter your First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter valide Email address")]
+        public string Email { get; set; }
+        [Required]
+        [Range(18,100)]
+        public int Age { get; set; }
+    }
+}
